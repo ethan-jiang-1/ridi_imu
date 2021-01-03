010d24de07d0121a324be71c9cdde454c8045e6a
ethanmac <ethanmac@mac.com>
2021-01-03 19:29:21 +0800

    align the code structure -- make it eaiser to compare

--- python/exam_gen_dataset.py
+++ python/exam_gen_dataset.py
@@ -22,6 +22,8 @@ if app_root not in sys.path:
 if app_root_python not in sys.path:
     sys.path.append(app_root_python)
 
+nano_to_sec = 1000000000.0
+#nano_to_sec = 1e09
 
 def interpolate_quaternion_linear(quat_data, input_timestamp, output_timestamp):
     """
@@ -131,13 +133,124 @@ def _find_root_dir_and_datalist(args):
     return root_dir, dataset_list
 
 
+def _exec_generate_one_dataset(args, data_root):
+    output_folder = data_root + '/processed'
+    if not os.path.isdir(output_folder):
+        os.makedirs(output_folder)
+    
+    # drop the head and tail
+    pose_data_all = np.genfromtxt(data_root+'/pose.txt')
+    pose_data = pose_data_all[args.skip_front:-args.skip_end, :]
+
+    # swap tango's orientation from [x,y,z,w] to [w,x,y,z]
+    pose_data[:, [-4, -3, -2, -1]] = pose_data[:, [-1, -4, -3, -2]]
+    # For some reason there might be a few duplicated records...
+    if not args.no_remove_duplicate:
+        unique_ts, unique_inds = np.unique(
+            pose_data[:, 0], return_index=True)
+        print('Portion of unique records: ',
+                unique_inds.shape[0] / pose_data.shape[0])
+        pose_data = pose_data[unique_inds, :]
+    
+    output_timestamp = pose_data[:, 0]
+    output_samplerate = output_timestamp.shape[0] * nano_to_sec / (output_timestamp[-1] - output_timestamp[0])
+    assert 195 < output_samplerate < 205, 'Wrong output sample rate: %f' % output_samplerate
+    print('Pose sample rate: {:2f}Hz'.format(output_samplerate))
+
+    gyro_data = np.genfromtxt(data_root+'/gyro.txt')
+    print('Gyroscope found. Sample rate:{:2f} Hz'.format((gyro_data.shape[0] - 1.0) * nano_to_sec / (gyro_data[-1, 0] - gyro_data[0, 0])))
+
+    acce_data = np.genfromtxt(data_root+'/acce.txt')
+    print('Acceleration found. Sample rate:{:2f} Hz'.format((acce_data.shape[0] - 1.0) * nano_to_sec / (acce_data[-1, 0] - acce_data[0, 0])))
+       
+    linacce_data = np.genfromtxt(data_root+'/linacce.txt')
+    print('Linear acceleration found. Sample rate:{:2f} Hz'.format((linacce_data.shape[0] - 1.0) * nano_to_sec / (linacce_data[-1, 0] - linacce_data[0, 0])))
+    
+    gravity_data = np.genfromtxt(data_root+'/gravity.txt')
+    print('Gravity found. Sample rate:{:2f} Hz'.format((gravity_data.shape[0] - 1.0) * nano_to_sec / (gravity_data[-1, 0] - gravity_data[0, 0])))
+
+    magnet_data = np.genfromtxt(data_root + '/magnet.txt')
+    print('Magnetometer: {:.2f}Hz'.format((magnet_data.shape[0] - 1.0) * nano_to_sec / (magnet_data[-1, 0] - magnet_data[0, 0])))
+
+
+    orientation_data = np.genfromtxt(data_root + '/orientation.txt')
+    # swap from x,y,z,w to w,x,y,z
+    orientation_data[:, [1, 2, 3, 4]] = orientation_data[:, [4, 1, 2, 3]]
+    print('Orientation found. Sample rate:{:2f}'.format((orientation_data.shape[0] - 1.0) * nano_to_sec / (orientation_data[-1, 0] - orientation_data[0, 0])))
+
+    # Generate dataset
+
+    # output_gyro_linear = interpolateAngularRateLinear(gyro_data, output_timestamp)
+    output_gyro_linear = interpolate_3dvector_linear(gyro_data[:, 1:], gyro_data[:, 0], output_timestamp)
+    output_accelerometer_linear = interpolate_3dvector_linear(acce_data[:, 1:], acce_data[:, 0], output_timestamp)
+    output_linacce_linear = interpolate_3dvector_linear(linacce_data[:, 1:], linacce_data[:, 0], output_timestamp)
+    output_gravity_linear = interpolate_3dvector_linear(gravity_data[:, 1:], gravity_data[:, 0], output_timestamp)
+    output_magnet_linear = np.zeros([output_timestamp.shape[0], 3])
+    if not args.no_magnet:
+        output_magnet_linear = interpolate_3dvector_linear(magnet_data[:, 1:], magnet_data[:, 0], output_timestamp)
+
+    # Convert rotation vector to quaternion
+    output_orientation = interpolate_quaternion_linear(orientation_data[:, 1:], orientation_data[:, 0], output_timestamp)
+
+    # construct a Pandas DataFrame
+    column_list = 'time,gyro_x,gyro_y,gyro_z,acce_x'.split(',') + \
+                  'acce_y,acce_z,linacce_x,linacce_y,linacce_z,grav_x,grav_y,grav_z'.split(',') + \
+                  'magnet_x,magnet_y,magnet_z'.split(',') + \
+                  'pos_x,pos_y,pos_z,ori_w,ori_x,ori_y,ori_z'.split(',') + \
+                  'rv_w,rv_x,rv_y,rv_z'.split(',')
+
+    data_mat = np.concatenate([output_timestamp[:, None], 
+                                output_gyro_linear,
+                                output_accelerometer_linear,
+                                output_linacce_linear,
+                                output_gravity_linear,
+                                output_magnet_linear,
+                                pose_data[:, 1:4],
+                                pose_data[:, -4:],
+                                output_orientation], axis=1)
+
+    data_pandas = pandas.DataFrame(data_mat, columns=column_list)
+    print(data_pandas)
+
+    # data_pandas.to_csv(output_folder + '/data.csv')
+    # print('Dataset written to ' + output_folder + '/data.txt')
+
+    # # write data in plain text file for C++
+    # with open(output_folder + '/data_plain.txt', 'w') as f:
+    #     f.write('{} {}\n'.format(data_mat.shape[0], data_mat.shape[1]))
+    #     for i in range(data_mat.shape[0]):
+    #         for j in range(data_mat.shape[1]):
+    #             f.write('{}\t'.format(data_mat[i][j]))
+    #         f.write('\n')
+
+    # if not args.no_trajectory:
+    #     from write_trajectory_to_ply import write_ply_to_file
+
+    #     print("Writing trajectory to ply file")
+    #     viewing_dir = np.zeros([data_mat.shape[0], 3], dtype=float)
+    #     viewing_dir[:, 2] = -1.0
+    #     write_ply_to_file(path=output_folder + '/trajectory.ply', position=pose_data[:, 1:4],
+    #                       orientation=pose_data[:, -4:])
+
+    if args.output_files:
+        _output_all_files(args, output_folder,
+                            data_pandas,  data_mat, pose_data)
+
+    # if args.clear_result:
+    #     command = 'rm -r %s/result*' % (data_root)
+    #     subprocess.call(command, shell=True)
+    _clean_result_file(args, data_root)
+
+    return data_pandas
+
+
 def _exec_generate_dataset(args):
 
     root_dir, dataset_list = _find_root_dir_and_datalist(args)
     print(root_dir)
     print(dataset_list)
 
-    nano_to_sec = 1000000000.0
+
     total_length = 0.0
     length_dict = {}
     for dataset in dataset_list:
@@ -152,116 +265,8 @@ def _exec_generate_dataset(args):
         data_root = root_dir + info[0]
         length = 0
 
-        print('------------------\nProcessing ' +
-                data_root, ', type: ' + motion_type)
-        # drop the head and tail
-        pose_data = np.genfromtxt(
-            data_root+'/pose.txt')[args.skip_front:-args.skip_end, :]
-
-        # swap tango's orientation from [x,y,z,w] to [w,x,y,z]
-        pose_data[:, [-4, -3, -2, -1]] = pose_data[:, [-1, -4, -3, -2]]
-        # For some reason there might be a few duplicated records...
-        if not args.no_remove_duplicate:
-            unique_ts, unique_inds = np.unique(
-                pose_data[:, 0], return_index=True)
-            print('Portion of unique records: ',
-                    unique_inds.shape[0] / pose_data.shape[0])
-            pose_data = pose_data[unique_inds, :]
-        output_timestamp = pose_data[:, 0]
-        output_samplerate = output_timestamp.shape[0] * nano_to_sec / (
-            output_timestamp[-1] - output_timestamp[0])
-        assert 195 < output_samplerate < 205, 'Wrong output sample rate: %f' % output_samplerate
-
-        print('Pose sample rate: {:2f}Hz'.format(output_samplerate))
-
-        acce_data = np.genfromtxt(data_root+'/acce.txt')
-        print('Acceleration found. Sample rate:{:2f} Hz'
-                .format((acce_data.shape[0] - 1.0) * nano_to_sec / (acce_data[-1, 0] - acce_data[0, 0])))
-        gyro_data = np.genfromtxt(data_root+'/gyro.txt')
-        print('Gyroscope found. Sample rate:{:2f} Hz'
-                .format((gyro_data.shape[0] - 1.0) * nano_to_sec / (gyro_data[-1, 0] - gyro_data[0, 0])))
-        linacce_data = np.genfromtxt(data_root+'/linacce.txt')
-        print('Linear acceleration found. Sample rate:{:2f} Hz'
-                .format((linacce_data.shape[0] - 1.0) * nano_to_sec / (linacce_data[-1, 0] - linacce_data[0, 0])))
-        gravity_data = np.genfromtxt(data_root+'/gravity.txt')
-        print('Gravity found. Sample rate:{:2f} Hz'
-                .format((gravity_data.shape[0] - 1.0) * nano_to_sec / (gravity_data[-1, 0] - gravity_data[0, 0])))
-
-        magnet_data = np.genfromtxt(data_root + '/magnet.txt')
-        print('Magnetometer: {:.2f}Hz'.
-                format((magnet_data.shape[0] - 1.0) * nano_to_sec / (magnet_data[-1, 0] - magnet_data[0, 0])))
-
-        orientation_data = np.genfromtxt(data_root + '/orientation.txt')
-        print('Orientation found. Sample rate:{:2f}'
-                .format((orientation_data.shape[0] - 1.0) * nano_to_sec /
-                        (orientation_data[-1, 0] - orientation_data[0, 0])))
-
-        output_folder = data_root + '/processed'
-        if not os.path.isdir(output_folder):
-            os.makedirs(output_folder)
-
-        # Generate dataset
-
-        # output_gyro_linear = interpolateAngularRateLinear(gyro_data, output_timestamp)
-        output_gyro_linear = interpolate_3dvector_linear(gyro_data[:, 1:], gyro_data[:, 0], output_timestamp)
-        output_accelerometer_linear = interpolate_3dvector_linear(acce_data[:, 1:], acce_data[:, 0],
-                                                                    output_timestamp)
-        output_linacce_linear = interpolate_3dvector_linear(linacce_data[:, 1:], linacce_data[:, 0],
-                                                            output_timestamp)
-        output_gravity_linear = interpolate_3dvector_linear(gravity_data[:, 1:], gravity_data[:, 0],
-                                                            output_timestamp)
-        output_magnet_linear = np.zeros([output_timestamp.shape[0], 3])
-        if not args.no_magnet:
-            output_magnet_linear = interpolate_3dvector_linear(magnet_data[:, 1:], magnet_data[:, 0],
-                                                                output_timestamp)
-        # swap from x,y,z,w to w,x,y,z
-        orientation_data[:, [1, 2, 3, 4]
-                            ] = orientation_data[:, [4, 1, 2, 3]]
-        # Convert rotation vector to quaternion
-        output_orientation = interpolate_quaternion_linear(orientation_data[:, 1:], orientation_data[:, 0],
-                                                            output_timestamp)
-
-        # construct a Pandas DataFrame
-        column_list = 'time,gyro_x,gyro_y,gyro_z,acce_x'.split(',') + \
-                        'acce_y,acce_z,linacce_x,linacce_y,linacce_z,grav_x,grav_y,grav_z'.split(',') + \
-                        'magnet_x,magnet_y,magnet_z'.split(',') + \
-                        'pos_x,pos_y,pos_z,ori_w,ori_x,ori_y,ori_z,rv_w,rv_x,rv_y,rv_z'.split(
-                            ',')
-        data_mat = np.concatenate([output_timestamp[:, None], output_gyro_linear,
-                                    output_accelerometer_linear,
-                                    output_linacce_linear,
-                                    output_gravity_linear,
-                                    output_magnet_linear,
-                                    pose_data[:, 1:4],
-                                    pose_data[:, -4:],
-                                    output_orientation], axis=1)
-
-        data_pandas = pandas.DataFrame(data_mat, columns=column_list)
-        print(data_pandas)
-
-        # data_pandas.to_csv(output_folder + '/data.csv')
-        # print('Dataset written to ' + output_folder + '/data.txt')
-
-        # # write data in plain text file for C++
-        # with open(output_folder + '/data_plain.txt', 'w') as f:
-        #     f.write('{} {}\n'.format(data_mat.shape[0], data_mat.shape[1]))
-        #     for i in range(data_mat.shape[0]):
-        #         for j in range(data_mat.shape[1]):
-        #             f.write('{}\t'.format(data_mat[i][j]))
-        #         f.write('\n')
-
-        # if not args.no_trajectory:
-        #     from write_trajectory_to_ply import write_ply_to_file
-
-        #     print("Writing trajectory to ply file")
-        #     viewing_dir = np.zeros([data_mat.shape[0], 3], dtype=float)
-        #     viewing_dir[:, 2] = -1.0
-        #     write_ply_to_file(path=output_folder + '/trajectory.ply', position=pose_data[:, 1:4],
-        #                       orientation=pose_data[:, -4:])
-
-        if args.output_files:
-            _output_all_files(args, output_folder,
-                                data_pandas,  data_mat, pose_data)
+        print('------------------\nProcessing ' + data_root, ', type: ' + motion_type)
+        data_pandas = _exec_generate_one_dataset(args, data_root)
 
         length = (data_pandas['time'].values[-1] -
                   data_pandas['time'].values[0]) / nano_to_sec
@@ -273,11 +278,6 @@ def _exec_generate_dataset(args):
         else:
             length_dict[motion_type] += length
         total_length += length
-
-        # if args.clear_result:
-        #     command = 'rm -r %s/result*' % (data_root)
-        #     subprocess.call(command, shell=True)
-        _clean_result_file(args, data_root)
 
     print('All done. Total length: {:.2f}s ({:.2f}min)'.format(
         total_length, total_length / 60.0))
--- python/exam_gen_dataset_nopose.py
+++ python/exam_gen_dataset_nopose.py
@@ -43,16 +43,20 @@ def _output_all_files(df_data, output_dir, data_mat):
             f.write('\n')
 
 
-def _exec_generate_dataset_nopose(args):
+def _exec_generate_one_dataset_nopose(args):
+    output_dir = args.dir + '/processed'
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
     gyro_input = np.genfromtxt(args.dir + '/gyro.txt')
     acce_input = np.genfromtxt(args.dir + '/acce.txt')
     linacce_input = np.genfromtxt(args.dir + '/linacce.txt')
     gravity_input = np.genfromtxt(args.dir + '/gravity.txt')
     magnet_input = np.genfromtxt(args.dir + '/magnet.txt')
+  
     rv_input_full = np.genfromtxt(args.dir + '/orientation.txt')
     rv_input = rv_input_full[args.skip_front:-args.skip_end]
-
+    # swap orientation from [x,y,z,w] to [w,x,y,z]
     rv_input[:, [1, 2, 3, 4]] = rv_input[:, [4, 1, 2, 3]]
 
     init_tango_quat = [math.sqrt(2.0)/2.0, math.sqrt(2.0)/2.0, 0.0, 0.0]
@@ -62,28 +66,28 @@ def _exec_generate_dataset_nopose(args):
         q = init_imu_quat * quaternion.quaternion(*rv_input[i, 1:])
         rv_input[i, 1:] = np.array([q.w, q.x, q.y, q.z])
 
-    output_dir = args.dir + '/processed'
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-
     nano_to_sec = 1e09
     output_timestamp = rv_input[:, 0]
-    output_samplerate = output_timestamp.shape[0] * nano_to_sec / (
-        output_timestamp[-1] - output_timestamp[0])
+    output_samplerate = output_timestamp.shape[0] * nano_to_sec / (output_timestamp[-1] - output_timestamp[0])
     assert 195 < output_samplerate < 205, 'Wrong output sample rate: %f' % output_samplerate
+    print('FkPose sample rate: {:2f}Hz'.format(output_samplerate))
 
     print('Processing gyro...')
     gyro_output = interpolate_3dvector_linear(gyro_input[:, 1:], gyro_input[:, 0], output_timestamp)
+
     print('Processing accelerometer...')
     acce_output = interpolate_3dvector_linear(acce_input[:, 1:], acce_input[:, 0], output_timestamp)
+
     print('Processing linear acceleration...')
     linacce_output = interpolate_3dvector_linear(linacce_input[:, 1:], linacce_input[:, 0],
                                                  output_timestamp)
     print('Processing gravity...')
     gravity_output = interpolate_3dvector_linear(gravity_input[:, 1:], gravity_input[:, 0],
                                                  output_timestamp)
+
     print('Processing magnetometer...')
     magnet_output = interpolate_3dvector_linear(magnet_input[:, 1:], magnet_input[:, 0], output_timestamp)
+
 
     fake_pose_data = np.zeros([rv_input.shape[0], 7], dtype=float)
     fake_pose_data[:, -4:] = init_tango_quat
@@ -91,11 +95,17 @@ def _exec_generate_dataset_nopose(args):
     column_list = 'time,gyro_x,gyro_y,gyro_z,acce_x'.split(',') + \
                   'acce_y,acce_z,linacce_x,linacce_y,linacce_z,grav_x,grav_y,grav_z'.split(',') + \
                   'magnet_x,magnet_y,magnet_z'.split(',') + \
-                  'pos_x,pos_y,pos_z,ori_w,ori_x,ori_y,ori_z,rv_w,rv_x,rv_y,rv_z'.split(
-                      ',')
+                  'pos_x,pos_y,pos_z,ori_w,ori_x,ori_y,ori_z'.split(',') + \
+                  'rv_w,rv_x,rv_y,rv_z'.split(',')
 
-    data_mat = np.concatenate([output_timestamp[:, None], gyro_output, acce_output, linacce_output, gravity_output,
-                               magnet_output, fake_pose_data, rv_input[:, 1:]], axis=1)
+    data_mat = np.concatenate([output_timestamp[:, None], 
+                               gyro_output, 
+                               acce_output, 
+                               linacce_output, 
+                               gravity_output,
+                               magnet_output, 
+                               fake_pose_data, 
+                               rv_input[:, 1:]], axis=1)
 
     df_data = pandas.DataFrame(data_mat, columns=column_list)
     if args.output_files:
@@ -126,4 +136,4 @@ if __name__ == '__main__':
 
     args = _fake_args(args)
 
-    _exec_generate_dataset_nopose(args)
+    _exec_generate_one_dataset_nopose(args)
